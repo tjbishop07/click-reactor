313e61c4a7bd32a93f90165451a3d8fef5d99d8c
tjbishop07 <tjbishop@gmail.com>
2019-08-12 10:03:46 -0700

    Delete dev.js

--- src/config/dev.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: 'AIzaSyD3h5o2yYDLrz7KpXXwWoicxthIocO41M8',
-    authDomain: 'clicky-71a02.firebaseapp.com',
-    databaseURL: 'https://clicky-71a02.firebaseio.com',
-    projectId: 'clicky-71a02',
-    storageBucket: 'clicky-71a02.appspot.com',
-    messagingSenderId: "123261595739"
-}
