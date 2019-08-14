65b84714ad67df7e4a1c9255c8a51f17528438ea
Tom Bishop <tom@honors-mbp.lan>
2019-08-13 18:04:38 -0700

    various ui updates and started building hud component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import List from './components/List';
+import Hud from './components/Hud';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Click Reactor</h1>
+        <h1 className="title">Click Reactor</h1>
+        <Hud />
         <List />
       </div>
     );
---
+++ src/components/Hud.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import _ from 'lodash';
+
+class Hud extends Component {
+
+  render() { 
+    const { data } = this.props;
+    const scores = [];
+    return (
+      <div>
+        {this.loadReactions()}
+        <div className="reactionBg"></div>
+        <div className="hud-container">
+          <h4>SCORE: {this.scores.reduce( function(count, reaction_score){ return count + reaction_score; }, 0)}</h4>
+          <h4>REACTIONS: {data ? _.map(data).length : '0'}/3</h4>
+        </div>
+      </div>
+    );
+  }
+
+  loadReactions = () => {
+    const { data } = this.props;
+    const x = _.map(data);
+    this.scores = _.map(x, 'clicks');
+  }
+
+}
+
+const mapStateToProps = ({ data }) => {
+  return {
+    data
+  }
+}
+
+export default connect(mapStateToProps, actions)(Hud);
