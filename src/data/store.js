b2371cf9892dbb5c7b5d41555c7cf7e7fadac67a
Tom Bishop <tom@Honors-MacBook-Pro.local>
2019-09-09 15:09:14 -0700

    more updates

---
+++ src/data/store.js
@@ -0,0 +1,86 @@
+export default [
+    {
+        id: 'sticks',
+        name: 'Rub Sticks',
+        basePrice: 10,
+        baseCPS: 0.2,
+        css: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)',
+    },
+    {
+        id: 'matches',
+        name: 'Matches',
+        basePrice: 20,
+        baseCPS: 0.4,
+        css: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)',
+    },
+    {
+        id: 'firecracker',
+        name: 'Firecracker',
+        basePrice: 30,
+        baseCPS: 0.6,
+        css: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)',
+    },
+    {
+        id: 'M80',
+        name: 'M80',
+        basePrice: 40,
+        baseCPS: 0.8,
+        css: 'linear-gradient(135deg, #E3FDF5 0%, #FFE6FA 100%)',
+    },
+    {
+        id: 'dynomite',
+        name: 'Dynomite',
+        basePrice: 100,
+        baseCPS: 1.5,
+        css: 'linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)',
+    },
+    {
+        id: 'c4',
+        name: 'C4',
+        basePrice: 500,
+        baseCPS: 3.0,
+        css: 'linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)',
+    },
+    // {
+    //     name: 'Ladoga Bottom',
+    //     description: '#ebc0fd → #d9ded8',
+    //     css: 'linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)',
+    //     height: 400
+    // },
+    // {
+    //     name: 'Sunny Morning',
+    //     description: '#f6d365 → #fda085',
+    //     css: 'linear-gradient(135deg, #f6d365 0%, #fda085 100%)',
+    //     height: 200
+    // },
+    // {
+    //     name: 'Lemon Gate',
+    //     description: '#96fbc4 → #f9f586',
+    //     css: 'linear-gradient(135deg, #96fbc4 0%, #f9f586 100%)',
+    //     height: 400
+    // },
+    // {
+    //     name: 'Salt Mountain',
+    //     description: ' #FFFEFF → #D7FFFE',
+    //     css: 'linear-gradient(135deg, #FFFEFF 0%, #D7FFFE 100%)',
+    //     height: 200
+    // },
+    // {
+    //     name: 'New York',
+    //     description: ' #fff1eb → #ace0f9',
+    //     css: 'linear-gradient(135deg, #fff1eb 0%, #ace0f9 100%)',
+    //     height: 400
+    // },
+    // {
+    //     name: 'Soft Grass',
+    //     description: ' #c1dfc4 → #deecdd',
+    //     css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
+    //     height: 400
+    // },
+    // {
+    //     name: 'Japan Blush',
+    //     description: ' #ddd6f3 → #faaca8',
+    //     css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
+    //     height: 200
+    // }
+]
