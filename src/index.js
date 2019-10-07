ea62f0cd1183f0cceb42990d29a6efe092138dee
Tom Bishop <tom@Honors-MacBook-Pro.local>
2019-10-07 14:43:36 -0700

    added update notifcation and enabled service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import TagManager from 'react-gtm-module'
+import './styles/index.scss';
 
 const tagManagerArgs = {
     gtmId: 'GTM-5F7T2ZF'
@@ -10,4 +11,30 @@ const tagManagerArgs = {
 
 TagManager.initialize(tagManagerArgs)
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.register();
+
+serviceWorker.register({
+    onUpdate: onUpdateHandler
+});
+
+function onUpdateHandler(registration) {
+
+    // Make sure that any new version of a service worker will take over the page 
+    // and become activated immediately.
+    const waitingServiceWorker = registration.waiting;
+    if (waitingServiceWorker) {
+        waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+    }
+
+    const link = document.createElement("a");
+    link.classList.add("update-notification");
+    link.setAttribute("href", "#");
+    link.innerHTML = "Update is available. Click here to install.";
+
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        window.location.reload(true);
+    });
+
+    console.log('ON UPDATE LINK', link);
+    document.querySelector('body').appendChild(link);
+}
